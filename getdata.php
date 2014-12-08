8a4454b10113a13519506f168dc9262f9a4f799e
Alvin Ong Xing Jui <alvinoxj@gmail.com>
2014-12-08 11:41:25 +0800

    edited db link

--- getdata.php
+++ getdata.php
@@ -2,8 +2,8 @@
 $appname = $_POST['appname'];
 
 try{
-	//$db = new PDO('mysql:host=localhost;dbname=teamabc;charset=utf8', 'root', 'toor');
-	$db = new PDO('mysql:host=localhost;dbname=koding_hackathon;charset=utf8', 'root', '');
+	$db = new PDO('mysql:host=localhost;dbname=teamabc;charset=utf8', 'root', 'toor');
+	//$db = new PDO('mysql:host=localhost;dbname=koding_hackathon;charset=utf8', 'root', '');
 	$stmt = $db->prepare('SELECT * FROM content WHERE supplier_name = :supplier_name');
 	$arr= array(':supplier_name' => $appname);
 	$stmt->execute($arr);

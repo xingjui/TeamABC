fa63c54d8a3b9fb2d5591f9a16190bbc29341a75
Alvin Ong Xing Jui <alvinoxj@gmail.com>
2014-12-08 15:52:16 +0800

    addsf

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-$id = $_POST['id'];
 $supplier_name = $_POST['supplier_name'];
 $name = $_POST['name'];
 $briefy = $_POST['briefy'];
@@ -7,8 +6,8 @@ $category = $_POST['category'];
 $explanation = $_POST['explanation'];
 
 try{
-	$db = new PDO('mysql:host=localhost;dbname=teamabc;charset=utf8', 'root', 'toor');
-	// $db = new PDO('mysql:host=localhost;dbname=koding_hackathon;charset=utf8', 'root', '');
+	// $db = new PDO('mysql:host=localhost;dbname=teamabc;charset=utf8', 'root', 'toor');
+	$db = new PDO('mysql:host=localhost;dbname=koding_hackathon;charset=utf8', 'root', '');
 	$stmt = $db->prepare('INSERT INTO content values (0, :supplier_name, :name, :briefy, :category, :explanation)');
 	$arr= array(':supplier_name' => $supplier_name, ':name' => $name, ':briefy' => $briefy, ':category' => $category, ':explanation' => $explanation);
 	if($stmt->execute($arr)){
